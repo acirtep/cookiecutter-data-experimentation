0bf6179ce7ff897a1511cc63c10d3e7e6e1114bf
acirtep <acirtep>
2023-04-02 22:05:24 +0200

    fix: add pytest and test case

--- {{cookiecutter.project_name}}/tests/test_app.py
+++ {{cookiecutter.project_name}}/tests/test_app.py
@@ -1,8 +1,11 @@
+import os
+
 import duckdb
 from {{cookiecutter.project_slug}}.app import get_data
 
 
 def test_get_data():
+    path = os.path.dirname(os.path.realpath(__file__))
     duckdb_conn = duckdb.connect()
-    df = get_data(duckdb_conn)
-    assert df.count() == 2
+    df = get_data(duckdb_conn, file=f'{path}/../data/example_data.csv')
+    assert df.shape[0] == 2
